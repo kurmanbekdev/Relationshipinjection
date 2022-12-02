902da58483bfafa26623ac095d566666c7eab4b2
07elbekdev <masrapovelbek24@gmail.com>
2022-12-02 10:29:05 +0600

    bgdfrsg

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,17 @@ public class Main {
         Scanner in = new Scanner(System.in);
         Scanner in1 = new Scanner(System.in);
         ArrayList<Users> all = new ArrayList<>();
-        mystery(in.nextInt(),in1);
+        mystery(in.nextInt(),in1.nextInt());
     }
-    public static void mystery(int b,int c){
+    public static void mystery(int b,String c){
         while (true) {
             System.out.println("1 -> save user" + "\n" + "2 -> delete user" + "\n" + "3 -> save user");
             int a = b;
             if (a == 1) {
                 System.out.print("name: ");
-                String name = in1.nextLine();
+                String name = c;
                 System.out.print("last name: ");
-                String lastName = in1.nextLine();
+                String lastName = c;
                 System.out.println("age: ");
                 int age = in.nextInt();
                 all.add(new Users(name, lastName, age));
